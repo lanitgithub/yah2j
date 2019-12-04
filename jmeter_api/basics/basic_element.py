680f038ebb2341de6713f7000e441683da120eed
32-52 <alexeysvetlov92@gmail.com>
2019-12-04 22:24:06 +0300

    Refactoring basicElement

--- jmeter_api/basics/basic_element.py
+++ jmeter_api/basics/basic_element.py
@@ -1,18 +1,51 @@
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 from settings import logging
 from settings import env
+import abc
+from abc import ABC, abstractmethod, abstractproperty
 
 
 class BasicElement():
     def __init__(self, name: str, comments: str, is_enable: bool):
         logging.debug(f'{type(self).__name__} | Init started...')
-        if not isinstance(is_enable, bool):
-            raise TypeError(
-                f'is_enable must be bool. is_enable {type(is_enable)} = {is_enable}')
         self.name = name
         self.comments = comments
-        self.is_enable = str(is_enable).lower()
+        self.is_enable = is_enable
         logging.debug(f'{type(self).__name__} | Init complited')
 
+        
+        pass
+
+    @abstractproperty
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @abstractproperty
+    def comments(self):
+        return self._comments
+
+    @comments.setter
+    def comments(self, value):
+        self._comments = value
+
+    @abstractproperty
+    def is_enable(self):
+        return self._is_enable
+
+    @is_enable.setter
+    def is_enable(self, value):
+        if not isinstance(value, bool):
+            raise TypeError(
+                f'is_enable must be bool. is_enable {type(value)} = {value}')
+        else:
+            self._is_enable = str(value).lower()
+
+
+class BasicElementJ2(BasicElement):
     def render_element(self) -> str:
         logging.debug(f'{type(self).__name__} | Render started...')
         template = env.get_template(f'{type(self).__name__}.j2')
--- jmeter_api/basics/test_basic_elements.py
+++ jmeter_api/basics/test_basic_elements.py
@@ -1,30 +1,30 @@
-from jmeter_api.basics.basic_element import BasicElement
+from jmeter_api.basics.basic_element import BasicElementJ2
 import xmltodict
 import pytest
 
 
 def test_basic_element_parameter_name():
-    element = BasicElement(name='DefaultName',
-                           comments='My |\][[] Element Comment',
-                           is_enable=True)
+    element = BasicElementJ2(name='DefaultName',
+                             comments='My |\][[] Element Comment',
+                             is_enable=True)
     render = element.render_element()
     var_name = xmltodict.parse(render)
     assert var_name['Arguments']['@testname'] == 'DefaultName'
 
 
 def test_basic_element_parameter_comments():
-    element = BasicElement(name='Default123Name',
-                           comments='My |\\][[] Element Comment',
-                           is_enable=False)
+    element = BasicElementJ2(name='Default123Name',
+                             comments='My |\\][[] Element Comment',
+                             is_enable=False)
     render = element.render_element()
     var_name = xmltodict.parse(render)
     assert var_name['Arguments']['stringProp']['#text'] == 'My |\\][[] Element Comment'
 
 
 def test_basic_element_parameter_is_enable():
-    element = BasicElement(name='DefaultwtjName',
-                           comments='Random Comment!',
-                           is_enable=True)
+    element = BasicElementJ2(name='DefaultwtjName',
+                             comments='Random Comment!',
+                             is_enable=True)
     render = element.render_element()
     var_name = xmltodict.parse(render)
     assert var_name['Arguments']['@enabled'] == 'true'
@@ -32,6 +32,6 @@ def test_basic_element_parameter_is_enable():
 
 def test_basic_element_parameter_is_enable_check():
     with pytest.raises(TypeError, match=r".*must be bool.*"):
-        BasicElement(name='DefaultwtjName',
-                     comments='Random Comment!',
-                     is_enable='True')
+        BasicElementJ2(name='DefaultwtjName',
+                       comments='Random Comment!',
+                       is_enable='True')
