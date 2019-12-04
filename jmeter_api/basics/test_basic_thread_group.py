8d015971d69c212fade72a414a3d41acfaf54bfb
32-52 <alexeysvetlov92@gmail.com>
2019-12-05 00:45:44 +0300

    Added basic_thread_group tests

---
+++ jmeter_api/basics/test_basic_thread_group.py
@@ -0,0 +1,18 @@
+from jmeter_api.basics.basic_thread_group import BasicThreadGroup, ThreadGroupAction
+import xmltodict
+import pytest
+
+
+def test_basic_thread_group_parameter_num_threads_type_check():
+    with pytest.raises(TypeError, match=r".*must be int.*"):
+        BasicThreadGroup(num_threads='qweasd')
+
+
+def test_basic_thread_group_parameter_num_threads_less_mone_check():
+    with pytest.raises(ValueError, match=r".*less than -1*"):
+        BasicThreadGroup(num_threads=-5)
+
+
+def test_basic_thread_group_parameter_num_threads_null_check():
+    with pytest.raises(ValueError, match=r".*must be more than 0*"):
+        BasicThreadGroup(num_threads=0)
