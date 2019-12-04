edb0f6669f2971ebe63cc1058b2e1de5fa5685bc
32-52 <alexeysvetlov92@gmail.com>
2019-12-04 22:38:27 +0300

    Added xml tests

--- jmeter_api/basics/test_basic_elements.py
+++ jmeter_api/basics/test_basic_elements.py
@@ -1,9 +1,9 @@
-from jmeter_api.basics.basic_element import BasicElementJ2
+from jmeter_api.basics.basic_element import BasicElementJ2, BasicElementXML
 import xmltodict
 import pytest
 
 
-def test_basic_element_parameter_name():
+def test_basic_element_j2_parameter_name():
     element = BasicElementJ2(name='DefaultName',
                              comments='My |\][[] Element Comment',
                              is_enable=True)
@@ -12,7 +12,7 @@ def test_basic_element_parameter_name():
     assert var_name['Arguments']['@testname'] == 'DefaultName'
 
 
-def test_basic_element_parameter_comments():
+def test_basic_element_j2_parameter_comments():
     element = BasicElementJ2(name='Default123Name',
                              comments='My |\\][[] Element Comment',
                              is_enable=False)
@@ -21,7 +21,7 @@ def test_basic_element_parameter_comments():
     assert var_name['Arguments']['stringProp']['#text'] == 'My |\\][[] Element Comment'
 
 
-def test_basic_element_parameter_is_enable():
+def test_basic_element_j2_parameter_is_enable():
     element = BasicElementJ2(name='DefaultwtjName',
                              comments='Random Comment!',
                              is_enable=True)
@@ -30,8 +30,41 @@ def test_basic_element_parameter_is_enable():
     assert var_name['Arguments']['@enabled'] == 'true'
 
 
-def test_basic_element_parameter_is_enable_check():
+def test_basic_element_j2_parameter_is_enable_check():
     with pytest.raises(TypeError, match=r".*must be bool.*"):
         BasicElementJ2(name='DefaultwtjName',
+                       comments='Random Comment!',
+                       is_enable='True')
+
+def test_basic_element_xml_parameter_name():
+    element = BasicElementXML(name='DefaultName',
+                             comments='My |\][[] Element Comment',
+                             is_enable=True)
+    render = element.render_element()
+    var_name = xmltodict.parse(render)
+    assert var_name['Arguments']['@testname'] == 'DefaultName'
+
+
+def test_basic_element_xml_parameter_comments():
+    element = BasicElementXML(name='Default123Name',
+                             comments='My |\\][[] Element Comment',
+                             is_enable=False)
+    render = element.render_element()
+    var_name = xmltodict.parse(render)
+    assert var_name['Arguments']['stringProp']['#text'] == 'My |\\][[] Element Comment'
+
+
+def test_basic_element_xml_parameter_is_enable():
+    element = BasicElementXML(name='DefaultwtjName',
+                             comments='Random Comment!',
+                             is_enable=True)
+    render = element.render_element()
+    var_name = xmltodict.parse(render)
+    assert var_name['Arguments']['@enabled'] == 'true'
+
+
+def test_basic_element_xml_parameter_is_enable_check():
+    with pytest.raises(TypeError, match=r".*must be bool.*"):
+        BasicElementXML(name='DefaultwtjName',
                        comments='Random Comment!',
                        is_enable='True')
