b9b736aa77066ed5c3c274cebea7146f8bdb7ee5
32-52 <alexeysvetlov92@gmail.com>
2019-12-09 23:24:29 +0300

    Added tests for max_elements_in_cache

--- jmeter_api/configs/http_cache_manager/test_http_cache_manager.py
+++ jmeter_api/configs/http_cache_manager/test_http_cache_manager.py
@@ -5,6 +5,9 @@ import pytest
 
 
 class TestHTTPCacheManager:
+    def minimal_positive(self):
+        HTTPCacheManager()
+
     class TestClearCacheEachIteration:
         def test_check(self):
             with pytest.raises(TypeError, match=r".*must be bool.*"):
@@ -30,3 +33,16 @@ class TestHTTPCacheManager:
         def test_positive(self):
             cache_manager = HTTPCacheManager(use_cache_control=False)
             assert cache_manager.use_cache_control == 'false'
+
+    class TestMaxElementsInCache:
+        def test_check(self):
+            with pytest.raises(TypeError, match=r".*must be int.*"):
+                HTTPCacheManager(max_elements_in_cache="test")
+
+        def test_check2(self):
+            with pytest.raises(TypeError, match=r".*must be int.*"):
+                HTTPCacheManager(max_elements_in_cache="120")
+
+        def test_positive(self):
+            cache_manager = HTTPCacheManager(max_elements_in_cache=100)
+            assert cache_manager.max_elements_in_cache == 100
