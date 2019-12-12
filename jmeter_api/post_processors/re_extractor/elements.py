7b6156f1d54a114021b5a8ddf60e1759814f12e3
32-52 <alexeysvetlov92@gmail.com>
2019-12-12 09:48:12 +0300

    Updated RegExpPostXML

--- jmeter_api/post_processors/re_extractor/elements.py
+++ jmeter_api/post_processors/re_extractor/elements.py
@@ -131,12 +131,10 @@ class RegExpPost(BasicPostProcessor):
 
 
 class RegExpPostXML(RegExpPost, Renderable):
+    root_element_name = 'RegexExtractor'
+    
     def render_element(self) -> str:
-        xml_tree: Optional[Element] = super().render_element()
-        element_root = xml_tree.find('RegexExtractor')
-
-        element_root.set('enabled', str(self.is_enabled).lower())
-        element_root.set('testname', self.name)
+        element_root, xml_tree = super().render_element()
         flag = True
         for element in list(element_root):
             try:
