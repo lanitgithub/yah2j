fc2f1e96f428d7934be196347f357f3343dbcb4c
32-52 <alexeysvetlov92@gmail.com>
2019-12-17 12:18:35 +0300

    Update example

--- test_plan_example1.py
+++ test_plan_example1.py
@@ -1,4 +1,9 @@
-from jmeter_api.elements import *
+from jmeter_api.configs.http_cache_manager.elements import HTTPCacheManager, FileEncoding
+from jmeter_api.timers.constant_throughput_timer.elements import ConstThroughputTimer, BasedOn
+from jmeter_api.timers.constant_timer.elements import ConstantTimer
+from jmeter_api.non_test_elements.test_plan.elements import TestPlan
+from jmeter_api.samplers.http_request.elements import HttpRequest, Method, Protocol, FileUpload, Source, Implement
+from jmeter_api.thread_groups.common_thread_group.elements import CommonThreadGroup, ThreadGroupAction
 
 if __name__ == "__main__":
     test_plan = TestPlan(name='NewTestPlan')
