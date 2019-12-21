e7c6e21b926a12c86f9a1e0f10c1fc49fd6ed93b
32-52 <alexeysvetlov92@gmail.com>
2019-12-21 14:37:54 +0300

    Added testsuite for common thread group

---
+++ jmeter_api/thread_groups/common_thread_group/test_common_thread_group.py
@@ -0,0 +1,24 @@
+from jmeter_api.thread_groups.common_thread_group.elements import CommonThreadGroup
+from jmeter_api.basics.utils import tag_wrapper
+import xmltodict
+import pytest
+
+
+class TestCommonThreadGroop:
+    class TestCommonThreadGroop:
+        def test_functional_mode_check(self):
+            with pytest.raises(TypeError):
+                CommonThreadGroup(functional_mode="True")
+
+        def test_teardown_on_shutdown_check(self):
+            with pytest.raises(TypeError):
+                CommonThreadGroup(teardown_on_shutdown="True")
+
+        def test_serialize_threadgroups_check(self):
+            with pytest.raises(TypeError):
+                CommonThreadGroup(serialize_threadgroups="True")
+
+    # class TestRenderCommonThreadGroop:
+    #     def test_parameter_num_threads(self):
+    #         btg = CommonThreadGroup(num_threads=10)
+    #         assert btg.num_threads == 10
