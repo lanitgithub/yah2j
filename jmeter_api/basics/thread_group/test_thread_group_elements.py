9b659ad46c0dd91e96016c79bd3296d172ef59dc
32-52 <alexeysvetlov92@gmail.com>
2019-12-22 22:06:57 +0300

    Deleted ThreadGroop old files

--- jmeter_api/basics/thread_group/test_thread_group_elements.py
+++ jmeter_api/basics/thread_group/test_thread_group_elements.py
@@ -2,40 +2,42 @@ from jmeter_api.basics.thread_group.elements import BasicThreadGroup, ThreadGrou
 import pytest
 
 
-class TestBasicThreadGroop:
-    class TestTreadGroop:
-        def test_basic_thread_group_parameter_num_threads(self):
+class TestBasicThreadGroopArgs:
+    class TestNumThreads:
+        def test_positive(self):
             btg = BasicThreadGroup(num_threads=10)
             assert btg.num_threads == 10
 
-        def test_basic_thread_group_parameter_num_threads_type_check(self):
+        def test_check(self):
             with pytest.raises(TypeError, match=r".*must be int.*"):
                 BasicThreadGroup(num_threads='qweasd')
 
-        def test_basic_thread_group_parameter_num_threads_less_more_check(self):
+        def test_less_more_check(self):
             with pytest.raises(ValueError, match=r".*less than -1*"):
                 BasicThreadGroup(num_threads=-5)
 
-        def test_basic_thread_group_parameter_num_threads_null_check(self):
+        def test_null_check(self):
             with pytest.raises(ValueError, match=r".*must be more than 0*"):
                 BasicThreadGroup(num_threads=0)
 
-        def test_basic_thread_group_parameter_on_sample_error(self):
+    class TestOnSampleError:
+        def test_positive(self):
             btg = BasicThreadGroup(on_sample_error=ThreadGroupAction.CONTINUE)
             assert btg.on_sample_error is ThreadGroupAction.CONTINUE
 
-        def test_basic_thread_group_parameter_on_sample_error_check(self):
+        def test_check(self):
             with pytest.raises(TypeError, match=r".*must be ThreadGroupAction*"):
                 BasicThreadGroup(on_sample_error='stopthread')
 
-        def test_basic_thread_group_parameter_ramp_time(self):
+    class TestRampTime:
+        def test_positive(self):
             btg = BasicThreadGroup(ramp_time=25)
             assert btg.ramp_time == 25
 
-        def test_basic_thread_group_parameter_ramp_time_check(self):
+        def test_check(self):
             with pytest.raises(TypeError, match=r".*must be int*"):
                 BasicThreadGroup(ramp_time='25')
 
-        def test_basic_thread_group_parameter_ramp_time_check2(self):
+        def test_check2(self):
             with pytest.raises(TypeError, match=r".*must be int*"):
                 BasicThreadGroup(ramp_time='random chars')
