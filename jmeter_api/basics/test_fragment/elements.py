04423029998460552fac3aa16b7ee1a9e9b27a0b
Пыхов Алексей <pykhov@lanit.ru>
2020-01-10 16:51:23 +0500

    TF check append

--- jmeter_api/basics/test_fragment/elements.py
+++ jmeter_api/basics/test_fragment/elements.py
@@ -1,6 +1,11 @@
 from jmeter_api.basics.element.elements import BasicElement
+from jmeter_api.basics.pre_processor.elements import BasicPreProcessor
+from jmeter_api.basics.post_processor.elements import BasicPostProcessor
 from jmeter_api.basics.controller.elements import BasicController
 from jmeter_api.basics.config.elements import BasicConfig
+from jmeter_api.basics.sampler.elements import BasicSampler
+from jmeter_api.basics.assertion.elements import BasicAssertion
+from jmeter_api.basics.listener.elements import BasicListener
 from jmeter_api.basics.sampler.elements import BasicSampler
 from jmeter_api.basics.timer.elements import BasicTimer
 from jmeter_api.basics.utils import IncludesElements
@@ -16,10 +21,12 @@ class BasicTestFragment(BasicElement, IncludesElements, ABC):
                          comments=comments,
                          is_enabled=is_enabled)
 
-    def append(self, new_element: Union[BasicSampler, BasicTimer, BasicConfig, BasicController]):
-        if not isinstance(new_element, (BasicSampler, BasicTimer, BasicConfig, BasicController)):
+    def append(self, new_element: Union[BasicSampler, BasicTimer, BasicConfig, BasicController,\
+                                        BasicPreProcessor, BasicPostProcessor, BasicAssertion, BasicListener]):
+        if not isinstance(new_element, (BasicSampler, BasicTimer, BasicConfig, BasicController,\
+                                        BasicPreProcessor, BasicPostProcessor, BasicAssertion, BasicListener)):
             raise TypeError(
-                f'new_element must be BasicSampler, BasicTimer, BasicConfig or BasicController. {type(new_element)} was given')
+                f'new_element must be BasicSampler, BasicTimer, BasicConfig, BasicPreProcessor, BasicPostProcessor, BasicAssertion, BasicListener or BasicController. {type(new_element)} was given')
         self._elements.append(new_element)
         return self
 
